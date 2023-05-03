2e3c7336d32d635874d43ea72789e3f4114e0b7f
Gord L <rndedg@gmail.com>
2023-05-03 10:09:42 -0700

    updated variable practice.

--- variables/variables.py
+++ variables/variables.py
@@ -1,36 +1,69 @@
-# variable = a container for a value. Behaves as the value it contains
-
-# Working with Strings
-
-# first_name = "Gord"
-# last_name = "Letkeman"
-# full_name = first_name + " " + last_name
-# type() prints what data type it is. ie String, Integer, Boolean
-# print(type(name))
-# print("Hello " + full_name)
-
-#---------------------------------
-
-# Working with Numbers
+# variable: a reusable container for storing a value
+#           behaves as if it were the value it contains
 
 # age = 37
-# print("Your age is: " + str(age))
-# age += 1
-# print("Next year you will be: " + str(age))
-# print(type(age))
+# print("You are " + str(age) + " years old")
+# print("You are", age, "years old")
+# # more popular way of doing this - fString(like a template literal in JS)
+# print(f"You are {age} years old")
 
-# Float - can store a decimal point, whereas a regular Int cannot
-
-# height = 177.8
-# print("Your height is: " + str(height) + "cm")
-# print(type(height))
-
-# Working with Booleans
-
-# human = True
-# print("Are you a human? " + str(human))
-# print(type(human))
+# INTEGER - whole numbers
+# age = 37
+# players = 2
+# quantity = 5
+#
+# print(f"You are {age} years old")
+# print(f"There are {players} players online")
+# print(f"You would like to buy {quantity} items")
 
 
-# Multiple Assignment - allows us to assign multiple variables at the same time in one line of code
+# FLOAT - numbers with a decimal
 
+# gpa = 3.4
+# distance = 2.5
+# price = 10.99
+#
+# print(f"Your gpa is {gpa}")
+# print(f"You ran {distance}Km")
+# print(f"The price is ${price}")
+
+# STRING
+
+# name = "Gord"
+# food = "pizza"
+# email = "helloWorld@gmail.com"
+#
+# print(f"Hello, {name}")
+# print(f"Your favorite food is {food}")
+# print(f"Your email is: {email}")
+
+# BOOLEAN - must be capitalized
+#         - typically run internally
+#         - not used within quotes
+
+# online = True
+# for_sale = False
+# running = False
+
+# print(f"Are you online?: {online}")
+# print(f"Is the item for sale?: {for_sale}")
+# print(f"Is the game running?: {running}")
+#
+# if running:
+#     print("The game is running")
+# else:
+#     print("The game is over")
+
+
+# Multiple Assignment - declare multiple variables on the same line
+# x = 1
+# y = 2
+# z = 3
+# independant values
+# x, y, z = 1, 2, 3
+# same value
+x = y = z = 0
+
+print(x)
+print(y)
+print(z)
